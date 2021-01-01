3d8a430419cc069ee8588b4763e42c39dc70c3d9
Elvan H <elvanhd@gmail.com>
2021-01-01 22:49:48 +0700

    explicit component names

---
+++ spring-demo-annotations/src/org/example/springdemo/TennisCoach.java
@@ -0,0 +1,13 @@
+package org.example.springdemo;
+
+import org.springframework.stereotype.Component;
+
+@Component("thatSillyCoach")
+public class TennisCoach implements Coach {
+
+  @Override
+  public String getDailyWorkout() {
+    return "Practice your backhand volley";
+  }
+
+}
