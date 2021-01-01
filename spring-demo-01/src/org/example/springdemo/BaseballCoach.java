f085a5745d477e673cce7a5f35a74cf7ef3823d5
Elvan H <elvanhd@gmail.com>
2021-01-01 14:04:19 +0700

    rough prototype part 2

--- spring-demo-01/src/org/example/springdemo/BaseballCoach.java
+++ spring-demo-01/src/org/example/springdemo/BaseballCoach.java
@@ -1,8 +1,10 @@
 package org.example.springdemo;
 
 public class BaseballCoach implements Coach {
+
   @Override
   public String getDailyWorkout() {
     return "Spend 30 minutes on batting practice";
   }
+
 }
