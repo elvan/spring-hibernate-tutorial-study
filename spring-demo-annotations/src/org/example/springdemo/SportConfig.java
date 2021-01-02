3cef71292a80e07f993a4a7b80ad62fe8805c1b2
Elvan H <elvanhd@gmail.com>
2021-01-02 12:09:59 +0700

    spring configuration with java code

---
+++ spring-demo-annotations/src/org/example/springdemo/SportConfig.java
@@ -0,0 +1,10 @@
+package org.example.springdemo;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("org.example.springdemo")
+public class SportConfig {
+
+}
