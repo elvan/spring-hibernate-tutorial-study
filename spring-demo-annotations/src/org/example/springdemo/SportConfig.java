d07ebb49fe3a24c5d58ca59d6f39ec0f79f500f7
Elvan H <elvanhd@gmail.com>
2021-01-02 14:15:03 +0700

    defining spring beans with java code

--- spring-demo-annotations/src/org/example/springdemo/SportConfig.java
+++ spring-demo-annotations/src/org/example/springdemo/SportConfig.java
@@ -1,10 +1,21 @@
 package org.example.springdemo;
 
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan("org.example.springdemo")
+// @ComponentScan("org.example.springdemo")
 public class SportConfig {
+  // define bean for our sad fortune service
+  @Bean
+  public FortuneService sadFortuneService() {
+    return new SadFortuneService();
+  }
+
+  // define bean for our swim coach AND inject dependency
+  @Bean
+  public Coach swimCoach() {
+    return new SwimCoach(sadFortuneService());
+  }
 
 }
