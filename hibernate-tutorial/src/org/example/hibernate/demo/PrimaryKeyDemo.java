38df9df0580532eec25d9a78c4f6be0f32ebc1c9
Elvan H <elvanhd@gmail.com>
2021-01-03 21:08:35 +0700

    primary keys

--- hibernate-tutorial/src/org/example/hibernate/demo/PrimaryKeyDemo.java
+++ hibernate-tutorial/src/org/example/hibernate/demo/PrimaryKeyDemo.java
@@ -5,7 +5,7 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-public class CreateStudentDemo {
+public class PrimaryKeyDemo {
 
   public static void main(String[] args) {
     // create session factory
@@ -18,16 +18,32 @@ public class CreateStudentDemo {
     Session session = factory.getCurrentSession();
 
     try {
-      // create a student object
-      System.out.println("Creating new student object");
-      Student tempStudent = new Student("Liam", "Noah", "liam@example.org");
+      // create 3 student objects
+      System.out.println("Creating 3 student objects");
+      Student tempStudent1 = new Student(
+        "Liam",
+        "Noah",
+        "liam@example.org"
+      );
+      Student tempStudent2 = new Student(
+        "Oliver",
+        "William",
+        "oliver@example.org"
+      );
+      Student tempStudent3 = new Student(
+        "Elijah",
+        "James",
+        "elijah@example.org"
+      );
 
       // start a transaction
       session.beginTransaction();
 
       // save the student object
       System.out.println("Saving the student");
-      session.save(tempStudent);
+      session.save(tempStudent1);
+      session.save(tempStudent2);
+      session.save(tempStudent3);
 
       // commit transaction
       session.getTransaction().commit();
