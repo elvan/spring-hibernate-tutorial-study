158c530fc2fac9e3412cb4dcb4ca45c22cb79303
Elvan H <elvanhd@gmail.com>
2021-01-04 22:18:14 +0700

    get courses for a student

--- hb-05-many-to-many/src/org/example/hibernate/demo/AddCoursesForMaryDemo.java
+++ hb-05-many-to-many/src/org/example/hibernate/demo/AddCoursesForMaryDemo.java
@@ -9,7 +9,7 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-public class CreateCourseForMaryDemo {
+public class AddCoursesForMaryDemo {
 
   public static void main(String[] args) {
     // create session factory
