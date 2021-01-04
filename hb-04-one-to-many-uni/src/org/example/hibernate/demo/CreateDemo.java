2d046d43ae215874ce4f3d6fb335ec791cfb85a8
Elvan H <elvanhd@gmail.com>
2021-01-04 20:32:13 +0700

    creating course reviews

---
+++ hb-04-one-to-many-uni/src/org/example/hibernate/demo/CreateDemo.java
@@ -0,0 +1,70 @@
+package org.example.hibernate.demo;
+
+import org.example.hibernate.demo.entity.Instructor;
+import org.example.hibernate.demo.entity.InstructorDetail;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class CreateDemo {
+
+  public static void main(String[] args) {
+    // create session factory
+    SessionFactory factory = new Configuration()
+      .configure("hibernate.cfg.xml")
+      .addAnnotatedClass(Instructor.class)
+      .addAnnotatedClass(InstructorDetail.class)
+      .buildSessionFactory();
+
+    // create a session
+    Session session = factory.getCurrentSession();
+
+    try {
+      // create the objects
+      Instructor tempInstructor1 = new Instructor(
+        "Jeremy",
+        "Williams",
+        "jeremy@gmail.com"
+      );
+      InstructorDetail tempInstructorDetail1 = new InstructorDetail(
+        "http://www.youtube.com/JeremyWilliams",
+        "Blogging"
+      );
+
+      Instructor tempInstructor2 = new Instructor(
+        "Paul",
+        "John",
+        "paul@gmail.com"
+      );
+      InstructorDetail tempInstructorDetail2 = new InstructorDetail(
+        "http://www.youtube.com/PaulJohn",
+        "Guitar"
+      );
+
+      // associate the objects
+      tempInstructor1.setInstructorDetail(tempInstructorDetail1);
+      tempInstructor2.setInstructorDetail(tempInstructorDetail2);
+
+      // start a transaction
+      session.beginTransaction();
+
+      // save the instructor
+      //
+      // Note: this will ALSO save the details object
+      // because of CascadeType.ALL
+      //
+      System.out.println("Saving instructor: " + tempInstructor1);
+      session.save(tempInstructor1);
+
+      System.out.println("Saving instructor: " + tempInstructor2);
+      session.save(tempInstructor2);
+
+      // commit transaction
+      session.getTransaction().commit();
+    }
+    finally {
+      factory.close();
+    }
+  }
+
+}
