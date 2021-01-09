2d2fbeffe139daab8b598e2b3c733dddb126212f
Elvan H <elvanhd@gmail.com>
2021-01-09 23:36:42 +0700

    json jackson demo

---
+++ jackson-databinding-json-demo/src/main/java/org/example/jackson/json/demo/Driver.java
@@ -0,0 +1,31 @@
+package org.example.jackson.json.demo;
+
+import java.io.File;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class Driver {
+
+  public static void main(String[] args) {
+    try {
+      // create object mapper
+      ObjectMapper mapper = new ObjectMapper();
+
+      // read JSON file and map/convert Java POJO
+      // data/sample-lite.json
+      Student student = mapper.readValue(
+        new File("data/sample-lite.json"),
+        Student.class
+      );
+
+      // print first name and last name
+      System.out.println("First name: " + student.getFirstName());
+      System.out.println("Last name: " + student.getLastName());
+    }
+    catch (Exception exc) {
+      exc.printStackTrace();
+    }
+
+  }
+
+}
