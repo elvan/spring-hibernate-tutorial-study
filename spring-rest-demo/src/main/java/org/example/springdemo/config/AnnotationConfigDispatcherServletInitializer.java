52727a2dd08d41cbd60316772c78f25b1affc5cb
Elvan H <elvanhd@gmail.com>
2021-01-10 13:31:29 +0700

    adding java config

---
+++ spring-rest-demo/src/main/java/org/example/springdemo/config/AnnotationConfigDispatcherServletInitializer.java
@@ -0,0 +1,28 @@
+package org.example.springdemo.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class AnnotationConfigDispatcherServletInitializer extends
+  AbstractAnnotationConfigDispatcherServletInitializer {
+
+  @Override
+  protected Class<?>[] getRootConfigClasses() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  protected Class<?>[] getServletConfigClasses() {
+    return new Class[] {
+      DemoAppConfig.class
+    };
+  }
+
+  @Override
+  protected String[] getServletMappings() {
+    return new String[] {
+      "/"
+    };
+  }
+
+}
---
+++ spring-rest-demo/src/main/java/org/example/springdemo/config/DemoAppConfig.java
@@ -0,0 +1,11 @@
+package org.example.springdemo.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("org.example.springdemo")
+public class DemoAppConfig {
+}
