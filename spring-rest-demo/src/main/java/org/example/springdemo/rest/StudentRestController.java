eb03c5e3cf11fdcc00f42815a1b8a8f1c6abd35d
Elvan H <elvanhd@gmail.com>
2021-01-10 21:13:34 +0700

    displaying one student

--- spring-rest-demo/src/main/java/org/example/springdemo/rest/StudentRestController.java
+++ spring-rest-demo/src/main/java/org/example/springdemo/rest/StudentRestController.java
@@ -7,6 +7,7 @@ import javax.annotation.PostConstruct;
 
 import org.example.springdemo.entity.Student;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,6 +28,11 @@ public class StudentRestController {
   @GetMapping("/students")
   public List<Student> getStudents() {
     return students;
+  }
+
+  @GetMapping("/students/{studentId}")
+  public Student getStudent(@PathVariable int studentId) {
+    return students.get(studentId);
   }
 
 }
