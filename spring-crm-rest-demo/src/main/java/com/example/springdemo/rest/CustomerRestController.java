ce3af5aac50539a0f5b0679c06ec3103b3a761d2
Elvan H <elvanhd@gmail.com>
2021-01-11 06:58:52 +0700

    deleting a customer

--- spring-crm-rest-demo/src/main/java/com/example/springdemo/rest/CustomerRestController.java
+++ spring-crm-rest-demo/src/main/java/com/example/springdemo/rest/CustomerRestController.java
@@ -3,6 +3,7 @@ package com.example.springdemo.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -51,6 +52,19 @@ public class CustomerRestController {
   public Customer updateCustomer(@RequestBody Customer customer) {
     customerService.saveCustomer(customer);
     return customer;
+  }
+
+  @DeleteMapping("/customers/{customerId}")
+  public String deleteCustomer(@PathVariable int customerId) {
+    Customer customer = customerService.getCustomer(customerId);
+
+    if (customer == null) {
+      throw new CustomerNotFoundException("Customer not found: " + customerId);
+    }
+
+    customerService.deleteCustomer(customerId);
+
+    return "Deleted customer with id: " + customerId;
   }
 
 }
