ff8f5dba012634141dc4ef4a5ea71a2dc8986d4b
Elvan H <elvanhd@gmail.com>
2021-01-12 00:31:09 +0700

    find and add employee

--- spring-boot-crud-demo/src/main/java/com/example/springboot/crud/rest/EmployeeRestController.java
+++ spring-boot-crud-demo/src/main/java/com/example/springboot/crud/rest/EmployeeRestController.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,6 +27,24 @@ public class EmployeeRestController {
   @GetMapping
   public List<Employee> findAll() {
     return employeeService.findAll();
+  }
+
+  @GetMapping("/{employeeId}")
+  public Employee getEmployee(@PathVariable int employeeId) {
+    Employee employee = employeeService.findById(employeeId);
+
+    if (employee == null) {
+      throw new RuntimeException("Employee id not found: " + employee);
+    }
+
+    return employee;
+  }
+
+  @PostMapping
+  public Employee addEmployee(@RequestBody Employee employee) {
+    employee.setId(0);
+    employeeService.save(employee);
+    return employee;
   }
 
 }
