bb394829eaffe8b756429202a75eeb07c35b55d3
Elvan H <elvanhd@gmail.com>
2021-01-12 04:14:10 +0700

    spring data rest config, sorting, and pagination

--- spring-boot-data-rest/src/main/java/com/example/springboot/crud/repository/EmployeeRepository.java
+++ spring-boot-data-rest/src/main/java/com/example/springboot/crud/repository/EmployeeRepository.java
@@ -1,8 +1,10 @@
 package com.example.springboot.crud.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.example.springboot.crud.entity.Employee;
 
+@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
