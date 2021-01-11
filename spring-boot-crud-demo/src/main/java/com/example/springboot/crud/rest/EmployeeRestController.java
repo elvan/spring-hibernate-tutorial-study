93f4831d63980bf4d85a660072c1818233b8a5df
Elvan H <elvanhd@gmail.com>
2021-01-12 00:36:18 +0700

    updating an employee

--- spring-boot-crud-demo/src/main/java/com/example/springboot/crud/rest/EmployeeRestController.java
+++ spring-boot-crud-demo/src/main/java/com/example/springboot/crud/rest/EmployeeRestController.java
@@ -3,9 +3,11 @@ package com.example.springboot.crud.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -44,6 +46,23 @@ public class EmployeeRestController {
   public Employee addEmployee(@RequestBody Employee employee) {
     employee.setId(0);
     employeeService.save(employee);
+    return employee;
+  }
+
+  @PutMapping
+  public Employee editEmployee(@RequestBody Employee employee) {
+    employeeService.save(employee);
+    return employee;
+  }
+
+  @DeleteMapping("/{employeeId}")
+  public Employee deleteEmployee(@PathVariable int employeeId) {
+    Employee employee = employeeService.findById(employeeId);
+
+    if (employee == null) {
+      throw new RuntimeException("Employee id not found: " + employee);
+    }
+
     return employee;
   }
 
